952be1f73ab1fd7816cf414f1bfd5f728bdc1a98
adisidev <64905594+adisidev@users.noreply.github.com>
2021-06-03 13:17:37 +0530

    Sorted which variables are needed in "MapState" and InsetState.

--- cartogram_generator/map_state.h
+++ cartogram_generator/map_state.h
@@ -13,16 +13,12 @@ struct XYPoint{
   double y;
 };
 
-class MapState {
+class InsetState {
 private:
   std::vector<GeoDiv> geo_divs_;
   std::map<std::string, double> target_areas;
   std::map<std::string, double> area_errs;
   std::map<std::string, Color> colors;
-  std::string id_header_;
-  std::string visual_variable_file_;
-  std::set<std::string> ids_in_visual_variables_file_;
-  bool is_world_map_;
   bool write_density_to_eps_;
   unsigned int lx_, ly_;  // Lattice dimensions
   unsigned int new_xmin_, new_ymin_; // To store map translation vector
@@ -32,10 +28,10 @@ private:
   fftw_plan fwd_plan_for_rho_, bwd_plan_for_rho_;
   unsigned int n_finished_integrations_;
   boost::multi_array<XYPoint, 2> proj_;
-  MapState();
+  InsetState();
 public:
-  explicit MapState(const std::string, const bool, const bool);
-  ~MapState();
+  explicit InsetState(const bool);
+  ~InsetState();
   unsigned int n_geo_divs() const;
   const std::vector<GeoDiv> geo_divs() const;
   std::vector<GeoDiv> *ref_to_geo_divs();
@@ -45,13 +41,6 @@ public:
   double target_areas_at(const std::string);
   bool target_area_is_missing(const std::string) const;
   const Color colors_at(const std::string);
-  bool colors_empty() const;
-  void set_id_header(const std::string);
-  const std::string id_header() const;
-  const std::string visual_variable_file() const;
-  void insert_id_in_visual_variables_file(const std::string);
-  const std::set<std::string> ids_in_visual_variables_file() const;
-  bool is_world_map() const;
   bool trigger_write_density_to_eps() const;
   void make_grid(const unsigned int, const unsigned int);
   unsigned int lx() const;
@@ -73,6 +62,26 @@ public:
   void set_area_errs();
   double area_errs_at(const std::string);
   double max_area_err();
+};
+
+class MapState {
+private:
+  std::vector<InsetState> inset_states_;
+  std::string id_header_;
+  std::string visual_variable_file_;
+  std::set<std::string> ids_in_visual_variables_file_;
+  bool is_world_map_;
+  bool write_density_to_eps_;
+public:
+  bool colors_empty() const;
+  void set_id_header(const std::string);
+  const std::string id_header() const;
+  const std::vector<GeoDiv> geo_divs() const;
+  const std::string visual_variable_file() const;
+  void insert_id_in_visual_variables_file(const std::string);
+  const std::set<std::string> ids_in_visual_variables_file() const;
+  bool is_world_map() const;
+  bool trigger_write_density_to_eps() const;
 };
 
 #endif
