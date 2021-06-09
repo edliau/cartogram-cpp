b4e499da72a9fea5b157a862265054e19baac3d9
adisidev <64905594+adisidev@users.noreply.github.com>
2021-06-10 00:16:29 +0530

    Added more testing scripts

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@
 # copy paste in to your terminal
 # replace original.json and template.csv with your data
 
+# to test data
 for d in */ ; do
   printf "Processing ${d}...\n"
   printf "=== ${d} ===\n\n" >> test_data.txt
@@ -12,3 +13,11 @@ for d in */ ; do
   printf "\n\n" >> test_data.txt
   printf "Done processing ${d}\n\n"
 done
+
+# to change data back to backup
+for d in */ ; do
+  mv ${d}template.csv.bak ${d}template.csv
+done
+
+# to change csv to NAME_1
+for i in */template.csv; do sed -i.bak '1 s/[A-Za-z0-9]*,/NAME_1,/' $i; done
