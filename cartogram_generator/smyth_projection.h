d8aff673a2c7e7149437735ca8b3a0e55cd3edf4
Phong Le <phongnguyen.le22@gmail.com>
2021-06-14 02:34:40 +0800

    Add smyth projection files

---
+++ cartogram_generator/smyth_projection.h
@@ -0,0 +1,10 @@
+#ifndef SMYTH_PROJECTION_H_
+#define SMYTH_PROJECTION_H_
+
+#include "inset_state.h"
+
+void project_to_smyth_equal_surface(InsetState*);
+
+void project_from_smyth_equal_surface(InsetState*);
+
+#endif
