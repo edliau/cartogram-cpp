f4ed2f90988cf567983806c900d02558833018f1
vuminhhieu <minhhieu7dams@gmail.com>
2021-12-15 20:58:08 +0800

    Attempt to integrate Cairo library into the project

--- cartogram_generator/main.cpp
+++ cartogram_generator/main.cpp
@@ -1,5 +1,5 @@
 // TO DO: positional matching of argument flags
-
+/*
 #include "albers_projection.h"
 #include "auto_color.h"
 #include "blur_density.h"
@@ -317,4 +317,31 @@ int main(const int argc, const char *argv[])
                 output_to_stdout,
                 &cart_info);
   return EXIT_SUCCESS;
+}
+*/
+
+#include <cairo.h>
+
+int main(void)
+{
+  cairo_surface_t *surface;
+  cairo_t *cr;
+
+  surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 390, 60);
+  cr = cairo_create(surface);
+
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+      CAIRO_FONT_WEIGHT_NORMAL);
+  cairo_set_font_size(cr, 40.0);
+
+  cairo_move_to(cr, 10.0, 50.0);
+  cairo_show_text(cr, "Disziplin ist Macht.");
+
+  cairo_surface_write_to_png(surface, "image.png");
+
+  cairo_destroy(cr);
+  cairo_surface_destroy(surface);
+
+  return 0;
 }
